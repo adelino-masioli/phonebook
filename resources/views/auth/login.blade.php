9e45a13e03051bab896df575c13a14a92ebf7fe9
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-03-31 12:34:02 +0100

    Page login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,86 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div class="col-md-7 col-left">
+            <h1> {{ __('Hello, how are you?') }}</h1>
+            
+            <div class="description">
+                <p>Enter your personal details</p>
+                <p>and start your journey with us</p>
             </div>
+
+            <a class="btn" href="{{ route('register') }}">
+                {{ __('Sign up') }}
+            </a>
+        </div>
+
+        <div class="col-md-5 col-right">
+
+            <h1> {{ __('Sign in to Phone Book') }}</h1>
+
+            <form method="POST" action="{{ route('login') }}" autocomplete="off">
+                @csrf
+
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <div class="form-wrapper">
+                            <i class="fa fa-envelope form-wrapper_icon"></i>
+                            <input   placeholder="{{ __('Email') }}"  id="email" type="email" class="form-wrapper_input" name="email" value="{{ old('email') }}" required autofocus autocomplete="off">
+                        </div>
+
+                       
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                {{ $errors->first('email') }}
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <div class="form-wrapper">
+                            <i class="fa fa-lock form-wrapper_icon"></i>
+                            <input  placeholder="{{ __('Password') }}" id="password" type="password" class="form-wrapper_input" name="password" required autocomplete="off">
+                        </div>
+
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                {{ $errors->first('password') }}
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="login-bottom">
+                    <div class="form-group row">
+                        <div class="col-md-12 text-center">
+                            <div class="custom-control custom-checkbox">
+                                <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="form-group row">
+                        <div class="col-md-12 text-center">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Sign in') }}
+                            </button>
+                        </div>
+
+                        <div class="col-md-12 text-center btn-forgot-password">
+                            @if (Route::has('password.request'))
+                                <a class="btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,23 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Phone Book') }}</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/92c34c34d1.js" crossorigin="anonymous"></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+</html>
